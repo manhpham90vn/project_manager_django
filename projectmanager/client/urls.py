1e8d7c77a73b1d39139e06da7b422487b66074f1
Manh Pham <manhpham90vn@icloud.com>
2024-01-29 14:31:36 +0700

    Add Client model and admin registration

--- projectmanager/client/urls.py
+++ projectmanager/client/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('update/<int:id>', views.update),
+    path('delete/<int:id>', views.delete),
 ]
--- projectmanager/client/views.py
+++ projectmanager/client/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Client
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the client index.")
+    clients = Client.objects.all()
+    return render(request, 'index.html', {'clients': clients})
+
+def update(request, id):
+    return HttpResponse("You're updating client %s." % id)
+
+def delete(request, id):
+    return HttpResponse("You're deleting client %s." % id)
