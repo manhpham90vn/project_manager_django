d1a2546c1b2d5d3673083f4002f068b8cb497bef
Manh Pham <manhpham90vn@icloud.com>
2024-02-01 17:48:25 +0700

    Add client templates and views

--- projectmanager/client/views.py
+++ projectmanager/client/views.py
@@ -5,12 +5,12 @@ from .forms import ClientForm
 
 # Create your views here.
 def index(request):
-    clients = Client.objects.all()
-    return render(request, 'index.html', {'clients': clients})
+    clients = Client.objects.all().order_by('id')
+    return render(request, 'client/index.html', {'clients': clients})
 
 def show(request, id):
     client = get_object_or_404(Client, pk=id)
-    return render(request, 'show.html', {'client': client})
+    return render(request, 'client/show.html', {'client': client})
 
 def new(request):
     if request.method == 'POST':
@@ -20,18 +20,18 @@ def new(request):
             return redirect('index')
     else:
         form = ClientForm()    
-    return render(request, 'new.html', {'form': form})
+    return render(request, 'client/new.html', {'form': form})
 
 def update(request, id):
     client = get_object_or_404(Client, pk=id)
     if request.method == 'POST':
-        form = ClientForm(request.POST)
+        form = ClientForm(request.POST, instance=client)
         if form.is_valid():
             form.save()
             return redirect('index')
     else:
         form = ClientForm(instance=client)    
-    return render(request, 'update.html', {'form': form})
+    return render(request, 'client/update.html', {'form': form})
 
 def delete(request, id):
     client = get_object_or_404(Client, pk=id)
